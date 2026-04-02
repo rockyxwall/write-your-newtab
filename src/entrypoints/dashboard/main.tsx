ffffce4baf5b41cd5180e3782375a83a603be894
rockyxwall <tanvird556@gmail.com>
2026-04-02 20:13:22 +0600

    installed shadcn/ui

--- src/entrypoints/dashboard/main.tsx
+++ src/entrypoints/dashboard/main.tsx
@@ -1,11 +1,39 @@
 // entrypoints/dashboard/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
   <h1 className="text-3xl font-bold underline bg-amber-600">
     Hello world! yoo
   </h1>
+  <Button variant="destructive">Button</Button>
+
+
+<Card>
+  <CardHeader>
+    <CardTitle>Card Title</CardTitle>
+    <CardDescription>Card Description</CardDescription>
+    <CardAction>Card Action</CardAction>
+  </CardHeader>
+  <CardContent>
+    <p>Card Content</p>
+  </CardContent>
+  <CardFooter>
+    <p>Card Footer</p>
+  </CardFooter>
+</Card>
+
+
   </React.StrictMode>,
 );
