6fb552c74797dbff85fd1803d2a08bbb313bcea1
rockyxwall <tanvird556@gmail.com>
2026-05-05 15:26:51 +0600

    feat: add WYNTab core structure with builtin templates, React dashboard, and project assets

--- wxt.config.ts
+++ wxt.config.ts
@@ -9,5 +9,12 @@ export default defineConfig({
       version: '0.2.1',
       action: {}, 
       permissions: ['tabs', 'storage', 'unlimitedStorage'],
+      icons: {
+        '16': 'favicon/favicon-96x96.png',
+        '32': 'favicon/favicon-96x96.png',
+        '48': 'favicon/favicon-96x96.png',
+        '96': 'favicon/favicon-96x96.png',
+        '128': 'favicon/web-app-manifest-192x192.png',
+      },
     },
 });
